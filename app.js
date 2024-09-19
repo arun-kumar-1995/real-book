b87164f64f953edb9480b584b4f5b750981531ce
arun kumar <arun.devpro@gmail.com>
2024-09-19 11:34:58 +0530

    necessary module imports

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import express from "express";
 import cookieParser from "cookie-parser";
 const app = express();
 
+import expressLayouts from "express-ejs-layouts";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the file path and directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
