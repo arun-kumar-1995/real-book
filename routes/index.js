eba472d119f05960371b77aff2b388b15332b5a8
arun kumar <arun.devpro@gmail.com>
2024-09-19 12:30:34 +0530

    added home route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const router = express.Router();
 // import routes
 import userRoutes from "./user.routes.js";
 // define routes
-router.use("/" , userRoutes);
+router.get("/", async (req, res, next) => {
+  return res.render("home", { title: "Real Book" });
+});
+
+router.use("/", userRoutes);
 //export default route
 export default router;
