5666e9bd6077b0ed82d8b4b3619a17883f41fb23
arun kumar <arun.devpro@gmail.com>
2024-09-19 20:44:53 +0530

    modified response handler for redirect

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -36,5 +36,43 @@ export const register = CatchAsyncError(async (req, res, next, session) => {
   // create user
   user = await User.create([{ name, email, password }], { session });
 
-  return SendResponse(res, 201, "User created", { user });
-});
+  return SendResponse(res, 201, "User created", { user }, "/sign-in");
+}, true);
+
+export const createSession = CatchAsyncError(
+  async (req, res, next, session) => {
+    const { email, password } = req.body;
+    // check for user existence
+    const user = await User.findOne({ email });
+    if (!user) return ErrorHandler(res, 404, "User not found");
+
+    // if user match passowrd
+    const match = await user.matchPassword(password);
+    if (!match) return ErrorHandler(res, 400, "Invalid email or password");
+
+    //get token
+    const token = getSignInToken(user._id);
+
+    //cookie option
+    const cookie_options = {
+      expires: new Date(
+        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    };
+
+    // create cookie
+    res.cookie(
+      "_session",
+      JSON.stringify({
+        token,
+        user: { role: user.role, id: user._id, username: user.username },
+      }),
+      cookie_options
+    );
+
+    return SendResponse(res, 200, "You are logged in", { token }, "/classroom");
+  },
+  true
+);
--- utils/responseHandler.utils.js
+++ utils/responseHandler.utils.js
@@ -1,9 +1,16 @@
-const SendResponse = (res, statusCode, message, data = null) => {
+const SendResponse = (
+  res,
+  statusCode,
+  message,
+  data = null,
+  redirect = null
+) => {
   return res.status(statusCode).json({
     success: statusCode >= 200 && statusCode < 300,
     message,
     status: statusCode,
     ...(data && { data }),
+    ...(redirect && { redirectUrl: redirect }),
   });
 };
 
