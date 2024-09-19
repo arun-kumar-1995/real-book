3a127703e19773b1de2eaf7615951d530ef624c0
arun kumar <arun.devpro@gmail.com>
2024-09-19 12:14:28 +0530

    testing default route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    success: true,
+    message: "Route was successfully called",
+  });
+});
 
 export default router;
