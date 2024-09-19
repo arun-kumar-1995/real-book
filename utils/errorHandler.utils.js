101755ed52d09ecde8bea0e7d4f21e0f83a375ca
arun kumar <arun.devpro@gmail.com>
2024-09-19 20:15:02 +0530

    added error handler helper function

---
+++ utils/errorHandler.utils.js
@@ -0,0 +1,10 @@
+const ErrorHandler = (res, statusCode, message) => {
+    res.status(statusCode).json({
+      success: false,
+      status: statusCode,
+      message,
+    });
+  };
+  
+  export default ErrorHandler;
+  
