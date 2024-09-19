05ca3268d3ddf1f429e1b492feb1976e8f56dd83
arun kumar <arun.devpro@gmail.com>
2024-09-19 11:32:16 +0530

    server started

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 import app from "./app.js";
 import db from "./config/db.config.js";
+
+const { PORT: port = 8000 } = process.env;
+
+// connect express app only if connected to db instance
+db.once("open", function () {
+    console.log("Connected to Database", db.host);
+    // Start the Express server
+    const expressServer = app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  });
