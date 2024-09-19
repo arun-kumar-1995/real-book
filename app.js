e24acb8f3b84afd724d7e07ad0556a7fcd0dcc87
arun kumar <arun.devpro@gmail.com>
2024-09-19 11:36:51 +0530

    setting up view engine and public directories

--- app.js
+++ app.js
@@ -14,4 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+// Use express-ejs-layouts middleware
+app.use(expressLayouts);
+
+// Set EJS as the template engine
+app.set("view engine", "ejs");
+
+// Set the directory for views
+app.set("views", path.join(__dirname, "./views"));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, "./public")));
+
+// serving static files
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 export default app;
