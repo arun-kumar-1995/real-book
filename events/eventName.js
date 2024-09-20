df0363d7a216f1d80c85a89746a56bfa833a42d2
arun kumar <arun.devpro@gmail.com>
2024-09-20 20:46:23 +0530

    defined few events

--- events/eventName.js
+++ events/eventName.js
@@ -1,5 +1,15 @@
 const Events = {
-  // EVENT_MESSAGE: "event message
+  CONNECT: "connect",
+  CONNECT_ERROR: "connect_error",
+  CONNECT_TIMEOUT: "connect_timeout",
+  DISCONNECT: "disconnect",
+  ERROR: "error",
+  MESSAGE: "message",
+  BROADCAST: "broadcast",
+  RECONNECT: "reconnect",
+  RECONNECT_ATTEMPT: "reconnect_attempt",
+  RECONNECT_ERROR: "reconnect_error",
+  RECONNECT_FAILED: "reconnect_failed",
 };
 
 export default Events;
