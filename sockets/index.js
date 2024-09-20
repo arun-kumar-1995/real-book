b73dbf43f9eb7934b94f98150c6e677bf24c2c7c
arun kumar <arun.devpro@gmail.com>
2024-09-20 19:38:21 +0530

    socket.io connection setup

---
+++ sockets/index.js
@@ -0,0 +1,24 @@
+import { Server } from "socket.io";
+
+const InitializeSocketConnection = (expressServer) => {
+  // setting up socket.io
+  const socketIo = new Server(expressServer, {
+    cors: {
+      origin: "http://localhost:5500",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  socketIo.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log(`User disconnected: ${socket.id}`);
+    });
+  });
+
+  // return socket instance
+  return socketIo;
+};
+
+export default InitializeSocketConnection;
