83c5ae7ba4bad7a68dde24c194f2cf2672d278f6
arun kumar <arun.devpro@gmail.com>
2024-09-22 17:57:48 +0530

    removed script for testing

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,31 @@
-// Initialize Socket.IO client
-//  global socket instance
-const socket = io();
+// // Initialize Socket.IO client
+// //  global socket instance
+// const socket = io();
 
-// connect socket
-socket.on("connect", () => {
-  console.log("Connected to Socket.IO server");
-});
+// // connect socket
+// socket.on("connect", () => {
+//   console.log("Connected to Socket.IO server");
+// });
 
-// disconnect event
-socket.on("disconnect", () => {
-  console.log("Disconnected from Socket.IO server");
-});
+// // disconnect event
+// socket.on("disconnect", () => {
+//   console.log("Disconnected from Socket.IO server");
+// });
 
-socket.on("error", (error) => {
-  console.error("Socket encountered error:", error);
-  // Take appropriate action, such as notifying the user
-});
+// socket.on("error", (error) => {
+//   console.error("Socket encountered error:", error);
+//   // Take appropriate action, such as notifying the user
+// });
 
-function getAvailableSeat(inputDate) {
-  socket.emit("seat-available", { inputDate });
-}
+// function getAvailableSeat(inputDate) {
+//   socket.emit("seat-available", { inputDate });
+// }
 
-// update classroom details
-socket.on("update-classroom", (data) => {
-  // busisness logic to update the classroom seat and details
-  const seatAnalytics = document.getElementById("seat-analytics");
-  updateSeatAnalysis(seatAnalytics, data);
-  // redraw classroom
-  setupClassroom((totalSeats = 36), (seatsPerRow = 8), data.socketData);
-});
+// // update classroom details
+// socket.on("update-classroom", (data) => {
+//   // busisness logic to update the classroom seat and details
+//   const seatAnalytics = document.getElementById("seat-analytics");
+//   updateSeatAnalysis(seatAnalytics, data);
+//   // redraw classroom
+//   setupClassroom((totalSeats = 36), (seatsPerRow = 8), data.socketData);
+// });
